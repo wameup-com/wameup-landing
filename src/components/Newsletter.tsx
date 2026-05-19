95e140cec9d60084d1dd7cd298559ab394237e48
WAMEUP <sima.u.dev@gmail.com>
2026-05-20 00:25:17 +0500

    Remove remaining old author references

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -27,7 +27,7 @@ export const Newsletter = () => {
           onSubmit={handleSubmit}
         >
           <Input
-            placeholder="leomirandadev@gmail.com"
+            placeholder="hello@wameup.com"
             className="bg-muted/50 dark:bg-muted/80 "
             aria-label="email"
           />
